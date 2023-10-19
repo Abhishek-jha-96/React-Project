904fc0f2a3da119b85328c2b234501f41c75fc0d
Abhishek Jha <abhishekjha96500@gmail.com>
2023-10-19 15:16:57 +0530

    Carhub App

---
+++ components/ShowMore.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { ShowMoreProps } from "@/types";
+import { useRouter } from "next/navigation";
+import { updateSearchParams } from "@/utils";
+import { CustomButton } from "@/components";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    // Calculate the new limit based on the page number and navigation type
+    const newLimit = (pageNumber + 1) * 10;
+
+    // Update the "limit" search parameter in the URL with the new value
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
+    
+    router.push(newPathname);
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnTypes="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ShowMore;
---
+++ utils/index.ts
@@ -0,0 +1,56 @@
+import { CarProps, FilterProps } from "@/types";
+
+export async function fetchCars( filters: FilterProps){
+    const { manufacturer, year, model, limit, fuel } = filters;
+    const headers = {
+        'X-RapidAPI-Key': '7834df3bf8msha8562f04d46df5ep157c90jsnc51180d0c3ae',
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+     
+    }
+
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel=${fuel}`, {
+        headers: headers,
+    });
+
+    const result = await response.json();
+
+    return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50;
+    const mileageFactor = 0.1;
+    const ageFactor = 0.05;
+
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0)
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+  
+    url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append('angle', `${angle}`);
+  
+    return `${url}`;
+  } 
+
+  export const updateSearchParams = (type: string, value: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    searchParams.set(type, value)
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+    return newPathname;
+  }
